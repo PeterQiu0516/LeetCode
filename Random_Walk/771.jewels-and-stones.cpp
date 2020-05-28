473a46da38f7a9f8b53a2c8397fb2c0ed8dde196
PeterQiu0516 <peterqiu0516@gmail.com>
2020-05-29 05:30:26 +0800

    Finish Ex. 771 Jewels and Stones

---
+++ Random_Walk/771.jewels-and-stones.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=771 lang=cpp
+ *
+ * [771] Jewels and Stones
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int numJewelsInStones(string J, string S)
+    {
+        map<char, int> Count;
+        for (auto s : S)
+        {
+            Count[s]++;
+        }
+
+        int res = 0;
+        for (auto j : J)
+        {
+            res += Count[j];
+        }
+        return res;
+    }
+};
+// @lc code=end
