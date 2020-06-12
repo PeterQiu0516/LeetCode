f3e78c3831f3512530c98f864498cd2dbfda220b
PeterQiu0516 <peterqiu0516@gmail.com>
2020-06-12 10:19:23 +0800

    Finish Ex. 933 basic queue

---
+++ VE280_InClassEx/933.number-of-recent-calls.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=933 lang=cpp
+ *
+ * [933] Number of Recent Calls
+ */
+
+// @lc code=start
+
+class RecentCounter
+{
+
+public:
+    RecentCounter()
+    {
+    }
+
+    int ping(int t)
+    {
+        q.push(t);
+        while (q.front() < (t - 3000))
+            q.pop();
+        return q.size();
+    }
+
+private:
+    queue<int> q;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+// @lc code=end
